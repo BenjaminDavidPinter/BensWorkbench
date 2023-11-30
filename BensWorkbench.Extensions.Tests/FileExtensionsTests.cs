9577878f6165a75222bc635d16f55080ee6f9793
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-11-30 00:22:49 -0500

    Abide by constraint model for test assertions

--- BensWorkbench.Extensions.Tests/FileExtensionsTests.cs
+++ BensWorkbench.Extensions.Tests/FileExtensionsTests.cs
@@ -4,7 +4,7 @@ public class FileExtensionTests
     [SetUp]
     public void Setup()
     {
-	    File.WriteAllText("TestFile.txt", "Hellow World!");
+        File.WriteAllText("TestFile.txt", "Hellow World!");
     }
 
     [Test]
@@ -22,23 +22,26 @@ public class FileExtensionTests
     }
 
     [Test]
-    public void TestValidRead() {
-    	var result = FileExtensions.ReadToBase64("TestFile.txt") switch {
-		var errRslt when errRslt.IsErr<Exception>() => false,
-		var okRslt when okRslt.IsOK() => true,
-		_ => false
-	};
+    public void TestValidRead()
+    {
+        var result = FileExtensions.ReadToBase64("TestFile.txt") switch
+        {
+            var errRslt when errRslt.IsErr<Exception>() => false,
+            var okRslt when okRslt.IsOK() => true,
+            _ => false
+        };
 
-	Assert.IsTrue(result);
+        Assert.IsTrue(result);
     }
 
     [Test]
-    public void TestWrite_Full() {
-    	File.WriteAllText("1.txt", "Test");
-	var result = FileExtensions.ReadToBase64("1.txt").Unwrap();
-	var result2 = FileExtensions.WriteBase64AsFile(result, "./", "2.txt").Unwrap();
-	var result3 = FileExtensions.ReadToBase64("2.txt").Unwrap();
+    public void TestWrite_Full()
+    {
+        File.WriteAllText("1.txt", "Test");
+        var result = FileExtensions.ReadToBase64("1.txt").Unwrap();
+        var result2 = FileExtensions.WriteBase64AsFile(result, "./", "2.txt").Unwrap();
+        var result3 = FileExtensions.ReadToBase64("2.txt").Unwrap();
 
-	Assert.AreEqual(result3, result);
-    } 
+        Assert.That(result3, Is.EqualTo(result));
+    }
 }
