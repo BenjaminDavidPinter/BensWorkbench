4edfd8fa235255ec39e790cb9486d3dd6b86fad0
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-11-30 00:17:24 -0500

    Test File writing tools

--- BensWorkbench.Extensions.Tests/FileExtensionsTests.cs
+++ BensWorkbench.Extensions.Tests/FileExtensionsTests.cs
@@ -4,6 +4,7 @@ public class FileExtensionTests
     [SetUp]
     public void Setup()
     {
+	    File.WriteAllText("TestFile.txt", "Hellow World!");
     }
 
     [Test]
@@ -19,4 +20,25 @@ public class FileExtensionTests
 
         Assert.IsTrue(result);
     }
+
+    [Test]
+    public void TestValidRead() {
+    	var result = FileExtensions.ReadToBase64("TestFile.txt") switch {
+		var errRslt when errRslt.IsErr<Exception>() => false,
+		var okRslt when okRslt.IsOK() => true,
+		_ => false
+	};
+
+	Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void TestWrite_Full() {
+    	File.WriteAllText("1.txt", "Test");
+	var result = FileExtensions.ReadToBase64("1.txt").Unwrap();
+	var result2 = FileExtensions.WriteBase64AsFile(result, "./", "2.txt").Unwrap();
+	var result3 = FileExtensions.ReadToBase64("2.txt").Unwrap();
+
+	Assert.AreEqual(result3, result);
+    } 
 }
--- BensWorkbench.Extensions.Tests/Usings.cs
+++ BensWorkbench.Extensions.Tests/Usings.cs
@@ -1,3 +1,4 @@
 global using NUnit.Framework;
 global using BensWorkbench.Extensions;
 global using BensWorkbench.Models;
+global using System.IO;
