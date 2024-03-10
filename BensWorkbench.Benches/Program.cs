d8c8af056cdeb3b079b29806248cb6e4fd483aba
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2024-03-10 15:14:41 -0400

    Added a Benchine suite

---
+++ BensWorkbench.Benches/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BensWorkbench.Models;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
